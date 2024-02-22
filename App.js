c63e7d0041004f4adfbcefe2684255ed19f58018
Palladino <gabriel.palladino@etec.sp.gov.br>
2024-02-22 10:41:42 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Gabriel Palladino</Text>
+      <br></br>
+      <Text>Desenvolvimento de Dispositivo Móveis</Text>
+      <br></br>
+      <Text>22/02/2024</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
